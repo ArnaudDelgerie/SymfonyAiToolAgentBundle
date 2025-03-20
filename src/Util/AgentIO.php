17df797abc8c91e5b6443d17118eba8e9ed324ca
arnaud <delgerie.arnaud@gmail.com>
2025-03-20 14:07:40 +0100

    clear code

--- src/Util/AgentIO.php
+++ src/Util/AgentIO.php
@@ -13,7 +13,7 @@ class AgentIO
         public string       $agentColor = 'blue'
     ) {}
 
-    public function ask(string $question, string $default = null, ?callable $validator = null): ?string
+    public function ask(string $question, ?string $default = null, ?callable $validator = null): ?string
     {
         $this->io->writeln('<fg='.$this->agentColor.'>'.$this->agentLabel.':</> '.$question);
         return $this->io->ask($this->userLabel, $default, $validator);
@@ -33,24 +33,6 @@ class AgentIO
     public function alert(string $content): void
     {
         $this->io->writeln('<fg='.$this->agentColor.'>'.$this->agentLabel.':</> <fg=red>'.$content.'</>'.PHP_EOL);
-    }
-
-    public function showAvailablesCommands(array $stopCommands): void
-    {
-        $this->text('Availables commands');
-        $table = $this->io->createTable();
-        $table->setStyle('box');
-        $table->setColumnWidth(0, 15);
-        $table->setColumnWidth(1, 45);
-        $table->setHeaders(['Command', 'Description']);
-        $tableRows = [];
-        /** @var AgentStopCommand $stopCommand */
-        foreach ($stopCommands as $stopCommand) {
-            $tableRows[] = [$stopCommand->word, $stopCommand->description];
-        }
-        $table->setRows($tableRows);
-        $table->render();
-        $this->io->text('');
     }
 
     public function logUsage(AgentUsageReport $usageReport): void
